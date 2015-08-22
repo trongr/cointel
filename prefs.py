e21710a8e7f0c1b13881d7d7e23aa420186b062d
trong <devgruxcon@gmail.com>
2015-08-22 07:15:23 -0400

    rank movies

--- prefs.py
+++ prefs.py
@@ -1,7 +1,7 @@
 # A dictionary of movie critics and their ratings of a small
 # set of movies
 prefs = {
-    'alice': {
+    'rose': {
         'Lady in the Water': 2.5,
         'Snakes on a Plane': 3.5,
         'Just My Luck': 3.0,
@@ -9,7 +9,12 @@ prefs = {
         'You, Me and Dupree': 2.5,
         'The Night Listener': 3.0
     },
-    'bob': {
+    'Toby': {
+        'Snakes on a Plane':4.5,
+        'You, Me and Dupree':1.0,
+        'Superman Returns':4.0
+    },
+    'matthews': {
         'Lady in the Water': 3.0,
         'Snakes on a Plane': 4.0,
         'The Night Listener': 3.0,
@@ -45,9 +50,4 @@ prefs = {
         'The Night Listener': 3.0,
         'You, Me and Dupree': 2.0
     },
-    'Toby': {
-        'Snakes on a Plane':4.5,
-        'You, Me and Dupree':1.0,
-        'Superman Returns':4.0
-    }
 }
--- similar.py
+++ similar.py
@@ -1,27 +1,56 @@
-#!/usr/bin/env
-from prefs import prefs
+#!/usr/bin/env python
+import prefs
 import numpy
 
+# Throws an error if a1 and a2 are not equal
 def similarity(a1, a2):
     if not a1 or not a2:
         return 0 # not similar
     return numpy.corrcoef(a1, a2)[0, 1]
 
-def scores(prefs, name1, name2):
+# scores for movies user1 and user2 have in common
+def common_scores(prefs, user1, user2):
     scores1 = []
     scores2 = []
-    for movie in prefs[name1]:
-        if movie in prefs[name2]:
-            scores1.append(prefs[name1][movie])
-            scores2.append(prefs[name2][movie])
+    for movie in prefs[user1]:
+        if movie in prefs[user2]:
+            scores1.append(prefs[user1][movie])
+            scores2.append(prefs[user2][movie])
     return scores1, scores2
 
-def prefSim(prefs, name1, name2):
-    scores1, scores2 = scores(prefs, name1, name2)
-    print similarity(scores1, scores2)
+def prefSimilarity(prefs, user1, user2):
+    scores1, scores2 = common_scores(prefs, user1, user2)
+    return similarity(scores1, scores2)
+
+# Returns the top 5 users with prefs similar to user, as a list of
+# pairs: [(score, user_name),...]
+def similarUsers(prefs, user, top_n_users=5):
+    matches = []
+    for other_user in prefs:
+        matches.append((prefSimilarity(prefs, user, other_user), other_user))
+    matches.sort()
+    matches.reverse()
+    return matches[0:top_n_users]
+
+def rankedMovies(prefs, user, top_n_users=5):
+    rankedUsers = similarUsers(prefs, user, top_n_users)
+    movie_weighted_scores = {}
+    for (user_sim_score, username) in rankedUsers:
+        for movie in prefs[username]:
+            weighted_score = user_sim_score * prefs[username][movie]
+            if weighted_score <= 0:
+                continue
+            movie_weighted_scores.setdefault(movie, list()).append((weighted_score, user_sim_score))
+    movie_scores = {}
+    for movie in movie_weighted_scores:
+        movie_total_score = sum([weighted_score for (weighted_score, user_sim_score) in movie_weighted_scores[movie]])
+        sim_sum = sum([user_sim_score for (weighted_score, user_sim_score) in movie_weighted_scores[movie]])
+        movie_scores[movie] = movie_total_score / sim_sum
+    return movie_scores
 
 def main():
-    prefSim(prefs, "alice", "bob")
+    print rankedMovies(prefs.prefs, "Toby", 10)
+    # print similarUsers(prefs.prefs, "Toby", 10)
 
 if __name__ == "__main__":
     main()
