c5929d75ade9669e3d5747ad7bd42a78dae5a9bc
trong <devgruxcon@gmail.com>
2015-08-28 04:24:19 -0400

    clustering

--- cluster.py
+++ cluster.py
@@ -2,6 +2,7 @@
 import feedparser
 import re
 import codecs
+import similar
 
 # removes tags and returns list of alphabetic (non numeric) words
 def getwords(html):
@@ -70,7 +71,7 @@ def readMatrixFile(matrixFile):
     lines = [line for line in file(matrixFile)]
     colnames = lines[0].strip().split('\t')[1:] # First line is the column titles
     rownames = []
-    data = []
+    data = [] # items are word counts in each blog
     for line in lines[1:]:
         p = line.strip().split('\t')
         rownames.append(p[0]) # First column in each row is the rowname
@@ -78,12 +79,68 @@ def readMatrixFile(matrixFile):
     return rownames, colnames, data
 
 class bicluster:
+    # vec contains data for this cluster, or the merged data of its
+    # children
     def __init__(self, vec, left=None, right=None, distance=0.0, id=None):
         self.left = left
         self.right = right
         self.vec = vec
+        # id is only used to attach the right blog name. todo:
+        # refactor so bicluster takes blog name as another param
         self.id = id
         self.distance = distance
+
+def hcluster(rows, distance):
+    currentclustid = -1
+    distances = {} # distances is the cache of distance calculations
+    clust = [bicluster(rows[i], id=i) for i in range(len(rows))] # Clusters are initially just the rows
+    ROW_LEN = len(clust[0].vec) # This is the total number of words in all the blogs
+
+    while len(clust) > 1:
+        lowestpair = (0, 1)
+        closest = distance(clust[0].vec, clust[1].vec)
+        # loop through every pair looking for the smallest distance
+        for i in range(len(clust)):
+            for j in range(i + 1, len(clust)):
+                if (clust[i].id, clust[j].id) not in distances:
+                    distances[(clust[i].id, clust[j].id)] = distance(clust[i].vec, clust[j].vec)
+
+                d = distances[(clust[i].id, clust[j].id)]
+                if d < closest:
+                    closest = d
+                    lowestpair = (i, j)
+
+        # calculate the average of the two closest clusters and merge them:
+        left = clust[lowestpair[0]]
+        right = clust[lowestpair[1]]
+        mergevec = [(left.vec[i] + right.vec[i]) / 2.0 for i in range(ROW_LEN)]
+        newcluster = bicluster(mergevec, left=left, right=right,
+                               distance=closest, id=currentclustid)
+
+        currentclustid -= 1 # cluster ids that weren't in the original set are negative
+        del clust[lowestpair[1]]
+        del clust[lowestpair[0]]
+        clust.append(newcluster)
+
+    return clust[0]
+
+def printclust(clust, labels=None, n=0):
+    # indent to make a hierarchy layout
+    for i in range(n):
+        print ' ',
+    if clust.id < 0: # negative id means that this is branch
+        print '-'
+    else: # positive id means that this is an endpoint
+        if labels == None:
+            print clust.id
+        else:
+            print labels[clust.id]
+
+    # now print the right and left branches
+    if clust.left != None:
+        printclust(clust.left, labels=labels, n=n+1)
+    if clust.right != None:
+        printclust(clust.right, labels=labels, n=n+1)
 
 def main():
     feedlist = "feedlist.txt"
@@ -92,7 +149,8 @@ def main():
     wordlist = makeWordList(apcount, wordcounts)
     makeWordMatrix(wordlist, wordcounts, matrixfile)
     rownames, colnames, data = readMatrixFile(matrixfile)
-    print rownames
+    clust = hcluster(data, distance=similar.pearsonDist)
+    printclust(clust, rownames)
 
 if __name__ == "__main__":
     main()
