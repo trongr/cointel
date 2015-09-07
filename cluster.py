880fba9111ff92b79bc74634fdce1e90a7cb842b
trong <devgruxcon@gmail.com>
2015-09-07 17:10:37 -0400

    2d visual

--- cluster.py
+++ cluster.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python
+import sys
 from PIL import Image, ImageDraw
 import feedparser
 import re
 import codecs
 import similar
 import random
+import math
 
 # k == number of centroids, i.e. the k in k-means
 # rows == [[m-dimensional point], [m-dimensional point],...]
@@ -277,9 +279,68 @@ def rotatematrix(data):
         newdata.append(newrow)
     return newdata
 
+def scaledown(data, distance=similar.pearsonDist, rate=0.01):
+  n=len(data)
+
+  # The real distances between every pair of items
+  realdist=[[distance(data[i],data[j]) for j in range(n)]
+             for i in range(0,n)]
+
+  # Randomly initialize the starting points of the locations in 2D
+  loc=[[random.random(),random.random()] for i in range(n)]
+  fakedist=[[0.0 for j in range(n)] for i in range(n)]
+
+  lasterror=None
+  for m in range(0,1000):
+    # Find projected distances
+    for i in range(n):
+      for j in range(n):
+        fakedist[i][j]=math.sqrt(sum([pow(loc[i][x]-loc[j][x],2)
+                                 for x in range(len(loc[i]))]))
+
+    # Move points
+    grad=[[0.0,0.0] for i in range(n)]
+
+    totalerror=0
+    for k in range(n):
+      for j in range(n):
+        if j==k: continue
+        # The error is percent difference between the distances
+        errorterm=(fakedist[j][k]-realdist[j][k])/realdist[j][k]
+
+        # Each point needs to be moved away from or towards the other
+        # point in proportion to how much error it has
+        grad[k][0]+=((loc[k][0]-loc[j][0])/fakedist[j][k])*errorterm
+        grad[k][1]+=((loc[k][1]-loc[j][1])/fakedist[j][k])*errorterm
+
+        # Keep track of the total error
+        totalerror+=abs(errorterm)
+    print "total error:", str(totalerror)
+
+    # If the answer got worse by moving the points, we are done
+    if lasterror and lasterror<totalerror: break
+    lasterror=totalerror
+
+    # Move each of the points by the learning rate times the gradient
+    for k in range(n):
+      loc[k][0]-=rate*grad[k][0]
+      loc[k][1]-=rate*grad[k][1]
+
+  return loc
+
+def draw2d(data,labels,jpeg='mds2d.jpg'):
+  img=Image.new('RGB',(2000,2000),(255,255,255))
+  draw=ImageDraw.Draw(img)
+  for i in range(len(data)):
+    x=(data[i][0]+0.5)*1000
+    y=(data[i][1]+0.5)*1000
+    draw.text((x,y),labels[i],(0,0,0))
+  img.save(jpeg,'JPEG')
+
 def main():
-    feedlist = "feedlist.txt"
+    feedlist = sys.argv[1]
     matrixfile = "wordmatrix.txt"
+    blog2d = "blog2d.jpg"
     apcount, wordcounts = getWordsFromFeedList(feedlist)
     wordlist = makeWordList(apcount, wordcounts)
     makeWordMatrix(wordlist, wordcounts, matrixfile)
@@ -297,10 +358,15 @@ def main():
     # # printclust(wordblog_clust, blognames)
     # drawdendrogram(wordblog_clust, words, jpeg='wordclust.jpg')
 
-    print "Clustering k-means"
-    bestMatches = kcluster(blogwords, distance=similar.pearsonDist, k=10)
-    print bestMatches
-    printKCluster(bestMatches, blognames)
+    # print "Clustering k-means"
+    # bestMatches = kcluster(blogwords, distance=similar.pearsonDist, k=10)
+    # print bestMatches
+    # printKCluster(bestMatches, blognames)
+
+    # TODO. use d3.js for better visuals
+    # Visualizing data in two dimensions
+    coords=scaledown(blogwords)
+    draw2d(coords,blognames,jpeg=blog2d)
 
 if __name__ == "__main__":
     main()
